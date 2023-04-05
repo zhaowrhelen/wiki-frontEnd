dfd1626fa24b0257d62afae0e97e18e81782fb99
zhaowrhelen <zhaowrhelen@gmail.com>
2023-04-04 17:46:11 -0700

    import icons + ebook layout

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,19 @@ import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
+import  * as  Icons from '@ant-design/icons-vue';
 
-createApp(App).use(store).use(router).use(Antd).mount('#app')
+const app = createApp(App);
+
+app.use(store).use(router).use(Antd).mount('#app');
+
+
+
+// all icons import in onetime
+const icons:any = Icons;
+
+for(const i in icons){
+    app.component(i,icons[i]);
+}
 
 
