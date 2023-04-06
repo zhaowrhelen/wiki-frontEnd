43adce41cf8be1c90b44f005068cacacf2e93f9e
zhaowrhelen <zhaowrhelen@gmail.com>
2023-04-05 18:12:15 -0700

    admin-ebook page

--- src/components/the-header.vue
+++ src/components/the-header.vue
@@ -1,15 +1,14 @@
 <template>
   <a-layout-header class="header">
     <div class="logo" />
-    <a-menu
-      theme="dark"
-      mode="horizontal"
-      v-model:selectedKeys="selectedKeys1"
-      :style="{ lineHeight: '64px' }"
-    >
-      <a-menu-item key="1">nav 1</a-menu-item>
-      <a-menu-item key="2">nav 2</a-menu-item>
-      <a-menu-item key="3">nav 3</a-menu-item>
+    <a-menu theme="dark" mode="horizontal" :style="{ lineHeight: '64px' }">
+      <a-menu-item key="1"><router-link to="/">Home</router-link></a-menu-item>
+      <a-menu-item key="2"
+        ><router-link to="/admin/ebook">Ebook Admin</router-link></a-menu-item
+      >
+      <a-menu-item key="3"
+        ><router-link to="/about">About Us</router-link></a-menu-item
+      >
     </a-menu>
   </a-layout-header>
 </template>
